2f3504f01f0254649ece9eb769e92bb1d948ca1f
yuyanjun <yuyanjun@zego.im>
2023-01-13 18:26:53 +0800

    cache gift play effects, play one by one

--- Flutter/virtual_gift_demo/lib/main.dart
+++ Flutter/virtual_gift_demo/lib/main.dart
@@ -10,6 +10,7 @@ import 'package:svgaplayer_flutter/parser.dart';
 import 'package:svgaplayer_flutter/player.dart';
 import 'package:http/http.dart' as http;
 import 'package:svgaplayer_flutter/proto/svga.pb.dart';
+
 // Package imports:
 import 'package:zego_uikit_prebuilt_live_audio_room/zego_uikit_prebuilt_live_audio_room.dart';
 
@@ -34,7 +35,7 @@ class HomePage extends StatelessWidget {
 
   /// Users who use the same liveID can join the same live audio room.
   final roomIDTextCtrl =
-  TextEditingController(text: Random().nextInt(10000).toString());
+      TextEditingController(text: Random().nextInt(10000).toString());
 
   @override
   Widget build(BuildContext context) {
@@ -122,11 +123,14 @@ class _LivePageState extends State<LivePage> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      subscriptions..add(
-          ZegoUIKit().getSignalingPlugin().getInRoomTextMessageStream().listen(
-              onInRoomTextMessageReceived))..add(
-          ZegoUIKit().getInRoomCommandReceivedStream().listen(
-              onInRoomCommandReceived));
+      subscriptions
+        ..add(ZegoUIKit()
+            .getSignalingPlugin()
+            .getInRoomTextMessageStream()
+            .listen(onInRoomTextMessageReceived))
+        ..add(ZegoUIKit()
+            .getInRoomCommandReceivedStream()
+            .listen(onInRoomCommandReceived));
     });
   }
 
@@ -139,7 +143,8 @@ class _LivePageState extends State<LivePage> {
   }
 
   // if you use reliable message channel, you need subscription this method.
-  void onInRoomTextMessageReceived(List<ZegoSignalingInRoomTextMessage> messages) {
+  void onInRoomTextMessageReceived(
+      List<ZegoSignalingInRoomTextMessage> messages) {
     debugPrint("onInRoomTextMessageStream:$messages");
     // You can display different animations according to gift-type
     var message = messages.first;
@@ -150,7 +155,8 @@ class _LivePageState extends State<LivePage> {
 
   // if you use unreliable message channel, you need subscription this method.
   void onInRoomCommandReceived(ZegoInRoomCommandReceivedData commandData) {
-    debugPrint("onInRoomCommandReceived, fromUser:${commandData.fromUser}, command:${commandData.command}");
+    debugPrint(
+        "onInRoomCommandReceived, fromUser:${commandData.fromUser}, command:${commandData.command}");
     // You can display different animations according to gift-type
     if (commandData.fromUser.id != localUserID) {
       GiftWidget.show(context, "assets/sports-car.svga");
@@ -163,7 +169,8 @@ class _LivePageState extends State<LivePage> {
       response = await http.post(
         //http://localhost:3000
         //https://zego-example-server-nextjs.vercel.app
-        Uri.parse('https://zego-example-server-nextjs.vercel.app/api/send_gift'),
+        Uri.parse(
+            'https://zego-example-server-nextjs.vercel.app/api/send_gift'),
         headers: {'Content-Type': 'application/json'},
         body: json.encode({
           'app_id': yourAppID,
@@ -182,7 +189,6 @@ class _LivePageState extends State<LivePage> {
         // the gift animation can start to be displayed
         GiftWidget.show(context, "assets/sports-car.svga");
       }
-
     } on Exception catch (error) {
       debugPrint("[ERROR], store fcm token exception, ${error.toString()}");
     }
@@ -191,65 +197,88 @@ class _LivePageState extends State<LivePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Stack(
-          children: [
-            ZegoUIKitPrebuiltLiveAudioRoom(
-              appID: yourAppID,
-              appSign: yourAppSign,
-              userID: localUserID,
-              userName: 'user_$localUserID',
-              roomID: widget.roomID,
-              config: (widget.isHost
-                  ? ZegoUIKitPrebuiltLiveAudioRoomConfig.host()
-                  : ZegoUIKitPrebuiltLiveAudioRoomConfig.audience())
-                ..takeSeatIndexWhenJoining = widget.isHost ? 0 : -1
-                ..bottomMenuBarConfig =
-                ZegoBottomMenuBarConfig(maxCount: 5, audienceExtendButtons: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(40, 40),
-                      shape: const CircleBorder(),
-                    ),
-                    onPressed: () {
-                      _sendGift();
-                    },
-                    child: const Icon(Icons.blender),
-                  )
-                ]),
-            ),
-          ],
-        ),
+      child: Stack(
+        children: [
+          ZegoUIKitPrebuiltLiveAudioRoom(
+            appID: yourAppID,
+            appSign: yourAppSign,
+            userID: localUserID,
+            userName: 'user_$localUserID',
+            roomID: widget.roomID,
+            config: (widget.isHost
+                ? ZegoUIKitPrebuiltLiveAudioRoomConfig.host()
+                : ZegoUIKitPrebuiltLiveAudioRoomConfig.audience())
+              ..takeSeatIndexWhenJoining = widget.isHost ? 0 : -1
+              ..bottomMenuBarConfig =
+                  ZegoBottomMenuBarConfig(maxCount: 5, audienceExtendButtons: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(40, 40),
+                    shape: const CircleBorder(),
+                  ),
+                  onPressed: () {
+                    _sendGift();
+                  },
+                  child: const Icon(Icons.blender),
+                )
+              ]),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class GiftWidget extends StatefulWidget {
-  const GiftWidget({Key? key, required this.onRemove, required this.giftPath}) : super(key: key);
-  final VoidCallback onRemove;
-  final String giftPath;
+  static OverlayEntry? currentGiftEntries;
+  static List<String> giftEntryPathCache = [];
 
-  static List<OverlayEntry> giftEntries = [];
-  static bool clear() {
-    for (final giftEntry in giftEntries) {
-      if (giftEntry.mounted) {
-        giftEntry.remove();
-      }
+  static void show(BuildContext context, String giftPath) {
+    if (null != currentGiftEntries) {
+      debugPrint("has gift displaying, cache $giftPath");
+
+      giftEntryPathCache.add(giftPath);
+
+      return;
     }
-    giftEntries.clear();
+
+    currentGiftEntries = OverlayEntry(builder: (context) {
+      return GiftWidget(
+        giftPath: giftPath,
+        onRemove: () {
+          if (currentGiftEntries?.mounted ?? false) {
+            currentGiftEntries?.remove();
+          }
+          currentGiftEntries = null;
+
+          if (giftEntryPathCache.isNotEmpty) {
+            var nextGiftPath = giftEntryPathCache.first;
+            giftEntryPathCache.removeAt(0);
+
+            debugPrint("has gift cache, play $nextGiftPath");
+
+            GiftWidget.show(context, nextGiftPath);
+          }
+        },
+      );
+    });
+
+    Overlay.of(context, rootOverlay: false)!.insert(currentGiftEntries!);
+  }
+
+  static bool clear() {
+    if (currentGiftEntries?.mounted ?? false) {
+      currentGiftEntries?.remove();
+    }
+
     return true;
   }
 
-  static void show(BuildContext context, String giftPath) {
-    late OverlayEntry giftEntry;
-    giftEntry = OverlayEntry(builder: (context) {
-      return GiftWidget(onRemove: () {
-        if (giftEntry.mounted) giftEntry.remove();
-        giftEntries.remove(giftEntry);
-      }, giftPath: giftPath,);
-    });
-    giftEntries.add(giftEntry);
-    Overlay.of(context, rootOverlay: false)!.insert(giftEntry);
-  }
+  const GiftWidget({Key? key, required this.onRemove, required this.giftPath})
+      : super(key: key);
+
+  final VoidCallback onRemove;
+  final String giftPath;
 
   @override
   State<GiftWidget> createState() => GiftWidgetState();
@@ -280,7 +309,9 @@ class GiftWidgetState extends State<GiftWidget>
         if (snapshot.hasData) {
           animationController ??= (SVGAAnimationController(vsync: this)
             ..videoItem = snapshot.data as MovieEntity
-            ..forward().whenComplete(() => widget.onRemove()));
+            ..forward().whenComplete(() {
+              widget.onRemove();
+            }));
           return SVGAImage(animationController!);
         } else if (snapshot.hasError) {
           return Text('${snapshot.error}');
@@ -291,4 +322,3 @@ class GiftWidgetState extends State<GiftWidget>
     );
   }
 }
-
